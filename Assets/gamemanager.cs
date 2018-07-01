8ebfd1723e192af6433f84332f34659d7b2fe665
Leonardo Almonti <leonardo.almonti@gmail.com>
2018-07-02 01:14:57 +0900

    Merge branch 'master' of https://github.com/donrafiki/coneugenio
    # Conflicts:
    #	Assets/Scenes/scene1.unity
    #	Assets/gamemanager.cs

--- Assets/gamemanager.cs
+++ Assets/gamemanager.cs
@@ -4,6 +4,10 @@ public class gamemanager : MonoBehaviour {
 
     public void speedup ()
     {
+<<<<<<< HEAD
+        Debug.Log("speedUP");
+=======
         Debug.Log("speedUP")
+>>>>>>> 618084671bcdd116127316ff0c2a6a6eebd150c5
     }
 }
